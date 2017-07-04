ed34c8759b411e8dd7ce8a1ec362d3910b3e277c
dennismuturia <dmuturia2922@gmail.com>
2017-07-04 11:16:16 +0300

    My commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>My first Angular JS</h1>
+    <div  *ngFor = 'let currentTask of tasks'>
+    <h3>{{currentTask.description}}</h3>
+    <button (click) = 'showDetails(currentTask)'>Click Me! To edit</button>
+    </div>
+    <h1>Edit tasks</h1>
+    <div  *ngIf="selectedTask">
+    <div>
+      <label>Enter Task</label>
+      <input [(ngModel)] = "selectedTask.description">
+    </div>
+    <div>
+      <label>Enter the ID</label>
+      <input [(ngModel)] = "selectedTask.id">
+      <button (click) = "finishEditing()">Finish editing</button>
+    </div>
+    </div>
+  </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[] = [
+    new Task("Create my first angularJS app", 0),
+    new Task("Learn more on Angular", 1),
+    new Task("Move on with life", 2)
+  ];
 
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+  finishEditing(){
+      this.selectedTask = null;
+  }
+}
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) { }
 }
